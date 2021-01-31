fc9437e37bd3e1bf9e3e16ad4104e3f1d77a65a9
antany <antany84@gmail.com>
2021-01-30 23:04:40 -0500

    removed else part

--- mylin.bat
+++ mylin.bat
@@ -3,8 +3,5 @@ vboxmanage list runningvms | find /i "mylin"
 if %errorlevel% == 1 (
   vboxmanage startvm mylin --type headless 
   timeout 15
-)else (  
-   echo "not running"
 )
-
 ssh antany@192.168.56.2
